9a2440119c58c23d5f82138d4ccbf8e5d8417123
Julian <chaparrobarrerajulian@gmail.com>
2026-04-08 12:53:06 -0500

    ok services viewfichasForSede

--- routes/api.php
+++ routes/api.php
@@ -64,7 +64,7 @@ Route::get('programas-por-municipio/{idMunicipio}',                        [Fich
 Route::get('fichas-por-programa-municipio/{idPrograma}/{idMunicipio}',     [FichaController::class,'fichasPorProgramaMunicipio']);
 
 Route::get('listarProgramasPorSede/{idSede}',[FichaController::class,'programasPorSede']);
-Route::get('fichasPorProgramaSede/{idPrograma}/{idMunicipio}',[FichaController::class,'fichasPorProgramaSede']);
+Route::get('fichasPorProgramaSede/{idPrograma}/{idMunicipio}',[FichaController::class,'ficharPorProgramaSede']);
 
 
 Route::get('listarSedes',[SedeController::class,'index']);
